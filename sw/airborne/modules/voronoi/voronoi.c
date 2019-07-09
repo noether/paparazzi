1150bfff6a7ef8065443b8c7ae1e639afb32dfe4
sergigraum <sergi.grau.m@gmail.com>
2019-07-09 08:11:57 +0200

    added centralized solution in voronoi.c module and a supervisor GCS that computes the coverage algorithm (.py)

--- sw/airborne/modules/voronoi/voronoi.c
+++ sw/airborne/modules/voronoi/voronoi.c
@@ -28,6 +28,8 @@
 #include "subsystems/navigation/common_nav.h"
 #include "autopilot.h"
 #include "std.h"
+#include "stdio.h"
+
 
 #if PERIODIC_TELEMETRY
 static void send_voronoi(struct transport_tx *trans, struct link_device *dev)
@@ -59,6 +61,8 @@ struct vor_con vor_control = {VOR_CENTRALIZED, VOR_RADIUS, VOR_TIMEOUT, VOR_BROA
 struct vor_tab vor_tables;
 
 uint32_t last_transmision = 0;
+//float map_vertices[4][2] = { {10,10}, {100,10}, {100,100}, {10,100}};
+
 
 void voronoi_init(void)
 {
@@ -71,11 +75,82 @@ void voronoi_init(void)
 #if PERIODIC_TELEMETRY
   register_periodic_telemetry(DefaultPeriodic, PPRZ_MSG_ID_VORONOI, send_voronoi);
 #endif
+// We should ask the GCS the map vertices
+//#if PERIODIC_TELEMETRY
+//  register_periodic_telemetry(DefaultPeriodic, PPRZ_MSG_ID_VORONOI, send_voronoi);
+//#endif
 }
+/* void get_voronoi_segments(map_vertices, sites, index)
+{
+
+}
+void get_intersections(voronoi_segments)
+{
+
+
+}
+
+void filter_vertices(map_vertices, sites, index)
+{
+  
+}
+
+void get_voronoi_cell(map_vertices, neighbors)
+{
+  // HOW I GET MY POSITION? I do not need it because we can use only the distances
+  // z = neighbor_distances
+  // sites = position stacked with neighbors
+  voronoi_segments = get_voronoi_segments(map_vertices, sites, index);
+  voronoi_vertices = get_intersections(voronoi_segments);
+
+  // filter vertices that are outside of the map and 
+  filtered_vertices = filter_vertices(map_vertices, voronoi_vertices, sites, index)
+
+}
+
+void get_centre_of_mass()
+{
+
+//return mass, centroid, tri;
+}
+
+void get_control_variable(float Kp=1, int density_function=1)
+{
+  mass, centroid, tri = get_centre_of_mass(voronoi_cell, density_function);
+  distance_to_centroid = centroid - position;
+  u = distance_to_centroid * Kp ;
+  // how is possible that the u is null or constant? whenever it reaches the 
+}
+
+void get_velocity()
+{
+
+}
+
+*/
 
 bool voronoi_main_loop(void)
 {
   uint32_t now = get_sys_time_msec();
+
+  if (VOR_CENTRALIZED)
+  {
+    // We only send the moving point from the python script
+    // We receive the position from the simulator
+    float radius_a = VOR_RADIUS;
+    float radius_b = VOR_RADIUS;
+    printf("Centralized %d: %f , %f \n", AC_ID,vor_control.xc, vor_control.yc);
+    //std::cout << "Centralized: " << vor_control.xc << ", " << vor_control.yc << std::endl;
+    gvf_ellipse_XY(vor_control.xc, vor_control.yc, radius_a, radius_b, 0);
+
+  }
+  else
+  {
+    printf("Not Centralized");
+
+
+
+  }
 
   // Table / Execute algorithm
   //for (int i = 0; i < DCF_MAX_NEIGHBORS; i++) {
@@ -108,7 +183,7 @@ bool voronoi_main_loop(void)
   // u *= -gvf_control.s * dcf_control.k;
 
   // Go and orbit around XY
-  // gvf_ellipse_XY(xc, yc, dcf_control.radius + u, dcf_control.radius + u, 0);
+  // gvf_ellipse_XY(xc, yc, dcf_control.radius, dcf_control.radius + u, 0);
 
   if ((now - last_transmision > vor_control.broadtime) && (autopilot_get_mode() == AP_MODE_AUTO2)) {
     send_vorinfo_to_nei();
@@ -137,8 +212,14 @@ void parseVorfromGCS(void)
 {
   if(vor_control.centralized)
   {
-    vor_control.xc = DL_VOR_GCS_AIR_xc(dl_buffer);
-    vor_control.yc = DL_VOR_GCS_AIR_yc(dl_buffer);
+    uint8_t ac_id = DL_VOR_GCS_AIR_ac_id(dl_buffer);
+    printf("AC_ID: %d \n", ac_id);
+    if (ac_id == AC_ID){
+      
+      printf("Buffer: %d \n", dl_buffer);
+      vor_control.xc = DL_VOR_GCS_AIR_xc(dl_buffer);
+      vor_control.yc = DL_VOR_GCS_AIR_yc(dl_buffer);
+    }
   }
 }
 
